97f698e6e8f216190e3885cec51be1b9d5ce7bc3
Ivan Gankevich <igankevich@yandex.com>
2015-03-24 16:52:08 +0300

    Removed old scripts.

--- src/test.sh
+++
@@ -1,7 +0,0 @@
-pkill test_discovery
-pkill test_discovery
-pkill test_discovery
-./test_discovery 127.0.0.1:60000 60000 60003 &
-./test_discovery 127.0.0.1:60001 60000 60003 &
-./test_discovery 127.0.0.1:60002 60000 60003 &
-wait
