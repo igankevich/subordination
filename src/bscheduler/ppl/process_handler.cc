2a38deeac3f51dee78b834fa332f4a0089ae3d19
Ivan Gankevich <igankevich@ya.ru>
2017-10-11 22:54:38 +0300

    WIP.

--- src/bscheduler/ppl/process_handler.cc
+++ src/bscheduler/ppl/process_handler.cc
@@ -12,16 +12,17 @@ bsc::process_handler<K,R>
 	}
 	this->log("_ _", __func__, event);
 	if (event.fd() == this->_outbuf->fd()) {
+		event.setev(sys::poll_event::Out);
 		this->_ostream.sync();
 	} else {
 		this->_istream.sync();
 		this->_proto.receive_kernels(this->_istream);
 	}
-	if (this->_outbuf->dirty()) {
-		event.setev(sys::poll_event::Out);
-	} else {
-		event.unsetev(sys::poll_event::Out);
-	}
+//	if (this->_outbuf->dirty()) {
+//		event.setev(sys::poll_event::Out);
+//	} else {
+//		event.unsetev(sys::poll_event::Out);
+//	}
 }
 
 template class bsc::process_handler<
