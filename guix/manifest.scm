ba605a7272f4eadc344ec77ea3e8b44d4f4d1e31
Ivan Gankevich <i.gankevich@spbu.ru>
2020-05-15 18:14:41 +0300

    Update guix manifest for CI.

--- guix/manifest.scm
+++ guix/manifest.scm
@@ -1,30 +1,46 @@
 (use-package-modules
-  base
-  build-tools
-  check
-  cmake
-  commencement
-  compression
-  gcc
-  ninja
-  pkg-config
-  unistdx
-  documentation
-  curl
-  graphviz
-  pre-commit)
+ base
+ build-tools
+ check
+ cmake
+ commencement
+ compression
+ gcc
+ ninja
+ pkg-config
+ unistdx
+ pre-commit
+ curl
+ graphviz
+ documentation
+ perl
+ tex
+ guile-xyz
+ groff
+ rsync
+ ssh
+ bash)
+
+(define sbn-manifest
+  (let ((v (getenv "SBN_MANIFEST")))
+    (if v (string-split v #\,) '())))
+
+(define (if-enabled name lst)
+  (if (member name sbn-manifest) lst '()))
 
 (packages->manifest
+ (append
   (list
-    unistdx
-    (list gcc-9 "lib")
-    gcc-toolchain-9
-    googletest
-    pkg-config
-    cmake
-    ninja
-    meson
-    python-pre-commit
-    zlib
-    ;; documentation
-    curl graphviz doxygen))
+   (@ (gnu packages unistdx) unistdx)
+   (list gcc-9 "lib")
+   gcc-toolchain-9
+   googletest
+   pkg-config
+   cmake
+   ninja
+   meson
+   python-pre-commit
+   zlib)
+  (if-enabled "site" (list curl graphviz doxygen perl texlive-bin haunt
+                           guile-syntax-highlight groff))
+  (if-enabled "ci" (list rsync openssh coreutils bash))))
