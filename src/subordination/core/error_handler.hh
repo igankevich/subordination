d8c8bfed0e93410e1ed3aeb877ab38e2f3a5c6a9
Ivan Gankevich <i.gankevich@spbu.ru>
2021-01-27 22:51:02 +0300

    #41 Use standard unexpected/terminate/signal handlers.

--- src/subordination/core/error_handler.hh
+++ src/subordination/core/error_handler.hh
@@ -3,14 +3,7 @@
 
 namespace sbn {
 
-    [[noreturn]] void
-    print_backtrace(int sig) noexcept;
-
-    [[noreturn]] void
-    print_error() noexcept;
-
-    void
-    install_error_handler();
+    void install_error_handler();
 
 }
 #endif // vim:filetype=cpp
